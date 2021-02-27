f0b26a81eeb2e2005b0b440d076038174020bbc6
Andre Baltieri <andrebaltieri@hotmail.com>
2021-02-27 11:42:50 -0300

    Iniciando o repo

---
+++ GravarExtension.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GravarExtension.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
